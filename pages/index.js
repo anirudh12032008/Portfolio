d6e966a887adee235ff70272c37c45ee905d8dab
Anirudh Sahu <renudilip19@gmail.com>
2022-07-01 19:28:02 +0530

    ' to &#39;

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,7 @@ export default function Home({ blogs, profile }) {
 
         <meta
           name="description"
-          content="This is Anirudh Sahu's PortFolio and Blog Website" />
+          content="This is Anirudh Sahu&#39;s PortFolio and Blog Website" />
 
         <link rel="icon" type="image/png" href="/assets/img/favicon.png" />
 
@@ -256,11 +256,11 @@ export default function Home({ blogs, profile }) {
             </div>
             <div className="pt-8 sm:pt-10 lg:pl-8 lg:pt-0">
               <h1 className="text-center font-header text-4xl text-white sm:text-left sm:text-5xl md:text-6xl">
-                Hello I'm {profile.name}!
+                Hello I&#39;m {profile.name}!
               </h1>
               <div className="flex flex-col justify-center pt-3 sm:flex-row sm:pt-5 lg:justify-start">
                 <div className="flex items-center justify-center pl-0 sm:justify-start md:pl-1">
-                  <p className="font-body text-lg uppercase text-white">Let's connect</p>
+                  <p className="font-body text-lg uppercase text-white">Let&#39;s connect</p>
                   <div className="hidden sm:block">
                     <i className="bx bx-chevron-right text-3xl text-yellow"></i>
                   </div>
@@ -378,7 +378,7 @@ export default function Home({ blogs, profile }) {
 
       <div className="container py-16 md:py-20" id="services">
         <h2 className="text-center font-header text-4xl font-semibold uppercase text-primary sm:text-5xl lg:text-6xl">
-          Here's what I'm good at
+          Here&#39;s what I&#39;m good at
         </h2>
         <h3 className="pt-6 text-center font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl">
           These are the services Ioffer
@@ -478,7 +478,7 @@ export default function Home({ blogs, profile }) {
           Check out my Portfolio
         </h2>
         <h3 className="pt-6 text-center font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl">
-          Here's what I have done with the past
+          Here&#39;s what I have done with the past
         </h3>
 
         <div className="mx-auto grid w-full grid-cols-1 gap-8 pt-12 sm:w-3/4 md:gap-10 lg:w-full lg:grid-cols-2">
@@ -758,7 +758,7 @@ export default function Home({ blogs, profile }) {
 
       <div className="container py-16 md:py-20" id="contact">
         <h2 className="text-center font-header text-4xl font-semibold uppercase text-primary sm:text-5xl lg:text-6xl">
-          Here's a contact form
+          Here&#39;s a contact form
         </h2>
         <h4 className="pt-6 text-center font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl">
           Have Any Questions?
@@ -835,7 +835,7 @@ export default function Home({ blogs, profile }) {
           <h3
             className="text-center font-header text-3xl uppercase leading-tight tracking-wide text-white sm:text-4xl lg:text-5xl">
             Keep <span className="font-bold">up-to-date</span> <br />
-            with what I'm up to
+            with what I&#39;m up to
           </h3>
           <form className="mt-6 flex flex-col justify-center sm:flex-row">
             <input className="w-full rounded px-4 py-3 font-body text-black sm:w-2/5 sm:py-4 lg:w-1/3" type="text"
