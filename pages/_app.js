737fe327abf546c2738aea5bed0936ad3babc8e7
Anirudh Sahu <renudilip19@gmail.com>
2022-07-01 20:31:12 +0530

    font issue

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,10 @@ import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   return <>
     <Head>
+    <link
+          crossOrigin="crossorigin"
+          href="https://fonts.gstatic.com"
+          rel="preconnect" />
       <link as="style"
         href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Raleway:wght@400;500;600;700&display=swap"
         rel="preload" />
