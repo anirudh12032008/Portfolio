52e09ffabb3c8ee1e06c66c48c4fcd984811e5e5
Anirudh Sahu <renudilip19@gmail.com>
2022-07-01 20:03:39 +0530

    added div

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -89,120 +89,120 @@ const Post = ({ blog, profile, author }) => {
       </>
     </Head>
     <NavBar profile={profile} />
-
     <div>
-      <div className="container py-6 md:py-10">
-        <div className="mx-auto max-w-4xl">
-          <div className="">
-            <h1
-              className="pt-5 font-body text-3xl font-semibold text-primary sm:text-4xl md:text-5xl xl:text-6xl">
-              {blog.title}
-            </h1>
-            <div className="flex items-center pt-5 md:pt-10">
-              <div>
-                <img
-                  src={`${builder.image(author.image).url() || '/assets/img/blog-author.jpg'}`}
-                  className="h-20 w-20 rounded-full border-2 border-grey-70 shadow"
-                  alt="author image" />
-              </div>
+      <div>
+        <div className="container py-6 md:py-10">
+          <div className="mx-auto max-w-4xl">
+            <div className="">
+              <h1
+                className="pt-5 font-body text-3xl font-semibold text-primary sm:text-4xl md:text-5xl xl:text-6xl">
+                {blog.title}
+              </h1>
+              <div className="flex items-center pt-5 md:pt-10">
+                <div>
+                  <img
+                    src={`${builder.image(author.image).url() || '/assets/img/blog-author.jpg'}`}
+                    className="h-20 w-20 rounded-full border-2 border-grey-70 shadow"
+                    alt="author image" />
+                </div>
 
 
-              <div className="pl-5">
-                <span className="block font-body text-xl font-bold text-grey-10">{author.title}</span>
-                <span className="block pt-1 font-body text-xl font-bold text-grey-30">{blog.createdAt}</span>
+                <div className="pl-5">
+                  <span className="block font-body text-xl font-bold text-grey-10">{author.title}</span>
+                  <span className="block pt-1 font-body text-xl font-bold text-grey-30">{blog.createdAt}</span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="prose max-w-none pt-8">
-            <PortableText
-              // Pass in block content straight from Sanity.io
-              content={blog.content}
-              projectId="2ft1oxvh"
-              dataset="production"
-              // Optionally override marks, decorators, blocks, etc. in a flat
-              // structure without doing any gymnastics
-              serializers={{
-                h1: (props) => <h1 {...props} />,
-                li: ({ children }) =><li className="special-list-item">{children}</li>,
-              }}
-            />
+            <div className="prose max-w-none pt-8">
+              <PortableText
+                // Pass in block content straight from Sanity.io
+                content={blog.content}
+                projectId="2ft1oxvh"
+                dataset="production"
+                // Optionally override marks, decorators, blocks, etc. in a flat
+                // structure without doing any gymnastics
+                serializers={{
+                  h1: (props) => <h1 {...props} />,
+                  li: ({ children }) => <li className="special-list-item">{children}</li>,
+                }}
+              />
 
-          </div>
-          <div className="mt-10 flex justify-between border-t border-lila py-12">
-            <Link href="/" className="flex items-center">
-              <i className="bx bx-left-arrow-alt text-2xl text-primary"></i>
-              <span
-                className="block pl-2 font-body text-lg font-bold uppercase text-primary md:pl-5">Previous
-                Post</span>
-            </Link>
-            <Link href="/" className="flex items-center">
-              <span className="block pr-2 font-body text-lg font-bold uppercase text-primary md:pr-5">Next
-                Post</span>
-              <i className="bx bx-right-arrow-alt text-2xl text-primary"></i>
-            </Link>
-          </div>
-          <div
-            className="flex flex-col items-center border-t border-lila py-12 pt-12 md:flex-row md:items-start xl:pb-20">
-            <div className="w-3/4 sm:w-2/5 lg:w-1/4 xl:w-1/5">
-              <img src={`${builder.image(author.image).url() || '/assets/img/blog-author.jpg'}`} className="rounded-full shadow" alt="author image" />
             </div>
-            <div className="ml-0 text-center md:ml-10 md:w-5/6 md:text-left">
-              <h3 className="pt-10 font-body text-2xl font-bold text-secondary md:pt-0">
-                {author.title}
-              </h3>
-              <p
-                className="pt-5 font-body text-lg leading-8 text-secondary sm:leading-9 md:text-xl md:leading-9 lg:leading-9 xl:leading-9">
-                {author.about}
+            <div className="mt-10 flex justify-between border-t border-lila py-12">
+              <Link href="/" className="flex items-center">
+                <i className="bx bx-left-arrow-alt text-2xl text-primary"></i>
+                <span
+                  className="block pl-2 font-body text-lg font-bold uppercase text-primary md:pl-5">Previous
+                  Post</span>
+              </Link>
+              <Link href="/" className="flex items-center">
+                <span className="block pr-2 font-body text-lg font-bold uppercase text-primary md:pr-5">Next
+                  Post</span>
+                <i className="bx bx-right-arrow-alt text-2xl text-primary"></i>
+              </Link>
+            </div>
+            <div
+              className="flex flex-col items-center border-t border-lila py-12 pt-12 md:flex-row md:items-start xl:pb-20">
+              <div className="w-3/4 sm:w-2/5 lg:w-1/4 xl:w-1/5">
+                <img src={`${builder.image(author.image).url() || '/assets/img/blog-author.jpg'}`} className="rounded-full shadow" alt="author image" />
+              </div>
+              <div className="ml-0 text-center md:ml-10 md:w-5/6 md:text-left">
+                <h3 className="pt-10 font-body text-2xl font-bold text-secondary md:pt-0">
+                  {author.title}
+                </h3>
+                <p
+                  className="pt-5 font-body text-lg leading-8 text-secondary sm:leading-9 md:text-xl md:leading-9 lg:leading-9 xl:leading-9">
+                  {author.about}
 
-              </p>
-              <div className="flex items-center justify-center pt-5 md:justify-start">
-                <Link href="/">
-                  <i className="bx bxl-facebook-square text-2xl text-primary hover:text-yellow"></i>
-                </Link>
-                <Link href="/" className="pl-4">
-                  <i className="bx bxl-twitter text-2xl text-primary hover:text-yellow"></i>
-                </Link>
-                <Link href="/" className="pl-4">
-                  <i className="bx bxl-dribbble text-2xl text-primary hover:text-yellow"></i>
-                </Link>
-                <Link href="/" className="pl-4">
-                  <i className="bx bxl-linkedin text-2xl text-primary hover:text-yellow"></i>
-                </Link>
-                <Link href="/" className="pl-4">
-                  <i className="bx bxl-instagram text-2xl text-primary hover:text-yellow"></i>
-                </Link>
+                </p>
+                <div className="flex items-center justify-center pt-5 md:justify-start">
+                  <Link href="/">
+                    <i className="bx bxl-facebook-square text-2xl text-primary hover:text-yellow"></i>
+                  </Link>
+                  <Link href="/" className="pl-4">
+                    <i className="bx bxl-twitter text-2xl text-primary hover:text-yellow"></i>
+                  </Link>
+                  <Link href="/" className="pl-4">
+                    <i className="bx bxl-dribbble text-2xl text-primary hover:text-yellow"></i>
+                  </Link>
+                  <Link href="/" className="pl-4">
+                    <i className="bx bxl-linkedin text-2xl text-primary hover:text-yellow"></i>
+                  </Link>
+                  <Link href="/" className="pl-4">
+                    <i className="bx bxl-instagram text-2xl text-primary hover:text-yellow"></i>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div className="bg-primary">
-      <div className="container flex flex-col justify-between py-6 sm:flex-row">
-        <p className="text-center font-body text-white md:text-left">
-          © Copyright 2022. All right reserved, Made with <i className="bx bx-heart text-white text-lg"></i> by Anirudh
-        </p>
-        <div className="flex items-center justify-center pt-5 sm:justify-start sm:pt-0">
-          <Link href={profile.facebook}>
-            <i className="bx bxl-facebook-square text-2xl text-white hover:text-yellow"></i>
-          </Link>
-          <Link href={profile.twitter} className="pl-4">
-            <i className="bx bxl-twitter text-2xl text-white hover:text-yellow"></i>
-          </Link>
-          <Link href={profile.youtube} className="pl-4">
-            <i className="bx bxl-dribbble text-2xl text-white hover:text-yellow"></i>
-          </Link>
-          <Link href={profile.discord} className="pl-4">
-            <i className="bx bxl-linkedin text-2xl text-white hover:text-yellow"></i>
-          </Link>
-          <Link href={profile.instagram} className="pl-4">
-            <i className="bx bxl-instagram text-2xl text-white hover:text-yellow"></i>
-          </Link>
+      <div className="bg-primary">
+        <div className="container flex flex-col justify-between py-6 sm:flex-row">
+          <p className="text-center font-body text-white md:text-left">
+            © Copyright 2022. All right reserved, Made with <i className="bx bx-heart text-white text-lg"></i> by Anirudh
+          </p>
+          <div className="flex items-center justify-center pt-5 sm:justify-start sm:pt-0">
+            <Link href={profile.facebook}>
+              <i className="bx bxl-facebook-square text-2xl text-white hover:text-yellow"></i>
+            </Link>
+            <Link href={profile.twitter} className="pl-4">
+              <i className="bx bxl-twitter text-2xl text-white hover:text-yellow"></i>
+            </Link>
+            <Link href={profile.youtube} className="pl-4">
+              <i className="bx bxl-dribbble text-2xl text-white hover:text-yellow"></i>
+            </Link>
+            <Link href={profile.discord} className="pl-4">
+              <i className="bx bxl-linkedin text-2xl text-white hover:text-yellow"></i>
+            </Link>
+            <Link href={profile.instagram} className="pl-4">
+              <i className="bx bxl-instagram text-2xl text-white hover:text-yellow"></i>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-
   </>
 }
 
