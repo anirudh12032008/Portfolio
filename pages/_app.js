ba6fc6b52c7b7792f6146e34ef50e8f38d4ee690
Anirudh Sahu <renudilip19@gmail.com>
2022-07-01 20:28:35 +0530

    font in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <link as="style"
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Raleway:wght@400;500;600;700&display=swap"
+        rel="preload" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
